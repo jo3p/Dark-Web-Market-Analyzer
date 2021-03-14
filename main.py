dc4a5ac7d9b87592a7d7d9ffa2e558583305ec3a
Joep <joep96@hotmail.com>
2021-03-14 15:53:08 +0100

    Extract drug names from drugpage

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
 import logging
-import pandas as pd
-
-from selenium.common.exceptions import NoSuchElementException, WebDriverException
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 from driver import create_webdriver
+from helpers import login
+
+USERNAME = 'sdfu3422532'
+PASSWORD = 'yu@53%sgsdfg67!te'
 
 logging.basicConfig(
     filename='logfile.log',
@@ -25,11 +26,35 @@ logging.getLogger('').addHandler(console)
 
 logging.info("Program launched.")
 
+# launch webdriver
 with create_webdriver() as driver:
     logging.info("Firefox browser launched.")
     try:
-        driver.get("http://counteuiwleiigv3.onion/")
-        logging.info("Successfully connected to a counterfeit store!")
+        driver.get("http://worldps45uh3rhedmx7g3jgjf3vw52wkvvcastfm46fzrpwoc7f33lid.onion/login")
+        logging.info("Successfully connected to the store!")
     except WebDriverException as e:
         logging.exception("Unable to load page. See attached exception traceback.")
-    time.sleep(15)
+
+    # login and return to homepage
+    login(driver, USERNAME, PASSWORD)
+
+    # checking main categories on marketplace
+    categories = {}
+    category_list = driver.find_elements_by_css_selector('[href*="/category/"]')
+    for category in category_list:
+        category_list_split = category.text.split('\n')
+        name = ''.join(filter(str.isalpha, category_list_split[0]))
+        amount = int(category_list_split[1])
+        categories[name] = category.get_attribute('href')
+
+    # navigate to drugs section
+    driver.get(categories['Drugs'])
+
+    # collect all drug categories in drugs section
+    drug_categories = {}  # dict containing all drug-categories (keys) and links (values)
+    for drug_category in driver.find_element_by_class_name('submenu').text.split('⩾')[1:]:
+        drug_name, drug_amount = drug_category.split('\n')[0].strip(), int(drug_category.split('\n')[1])
+        drug_categories[drug_name] = int(drug_amount)
+    drug_names = list(drug_categories.keys())  # list containing all drug names as stripped strings
+
+    time.sleep(100)
