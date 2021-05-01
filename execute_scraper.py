ca6cd5d9d08d1e153d9c8ee2420a629e20356387
forensics <jarno.e.smit@gmail.com>
2021-05-01 14:15:41 +0200

    Outline

--- execute_scraper.py
+++ execute_scraper.py
@@ -6,10 +6,8 @@ from helpers.log import init_logging
 if __name__ == '__main__':
     logging = init_logging()
     start_time = time()
-
     parser = DocumentParser(verbose=True)
     parser.scrape(scrape_listings=True, scrape_sellers=False)
     parser.save_as_parquet()
-
     parser.save_to_sql_db()
     logging.info(f'Finished parsing! Total execution time: {time() - start_time}s')
